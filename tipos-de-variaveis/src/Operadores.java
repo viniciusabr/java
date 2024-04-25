9efec6d9226a3da4413900e08271c821ead48425
Vinícius <viniciusabreu2001@gmail.com>
2024-04-25 17:47:25 -0300

    operadores

---
+++ tipos-de-variaveis/src/Operadores.java
@@ -0,0 +1,22 @@
+public class Operadores {
+    public static void main(String[] args){
+        // operadores lógicos
+
+        // && -> "E" lógico
+        // || -> "OU" lógico
+
+        boolean condicao1= true;
+        boolean condicao2= false;
+
+        if (condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        }// "E"
+        if (condicao1 || condicao2){
+            System.out.println("uma das condições é verdadeira ou as duas são verdadeira");
+        }// "OU"
+
+
+
+
+}
+}
